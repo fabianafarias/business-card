df0231f9a847e90c98781ba2bffdcec8cf683a31
Fabiana Farias <farias.fabianam@gmail.com>
2021-07-14 19:53:43 -0300

    Configurando Room database: criando entidade, criando dao, criando class repository, criando classe database

--- app/src/main/java/br/com/fabianafarias/businesscard/App.kt
+++ app/src/main/java/br/com/fabianafarias/businesscard/App.kt
@@ -1,7 +1,11 @@
 package br.com.fabianafarias.businesscard
 
 import android.app.Application
+import br.com.fabianafarias.businesscard.data.AppDatabase
+import br.com.fabianafarias.businesscard.data.BusinessCardRepository
 
 class App : Application() {
 
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
