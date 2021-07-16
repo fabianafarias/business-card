b05bd352b2c892068897594fb98955462b4e7686
Fabiana Farias <farias.fabianam@gmail.com>
2021-07-15 21:30:03 -0300

    Configurando Room: concluindo a ViewModel

--- app/src/main/java/br/com/fabianafarias/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/fabianafarias/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,11 +2,20 @@ package br.com.fabianafarias.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import br.com.fabianafarias.businesscard.App
+import br.com.fabianafarias.businesscard.R
+import br.com.fabianafarias.businesscard.data.BusinessCard
 import br.com.fabianafarias.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +29,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
         }
 
         binding.btnConfirm.setOnClickListener {
-
+            val businessCard = BusinessCard(
+                nome = binding.tilNome.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
         }
 
     }
